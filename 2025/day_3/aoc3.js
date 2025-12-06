9865e481affb28609ca9bf608b613c67451c3cd8
slamtotten <slamtotten@gmail.com>
2025-12-06 23:46:14 +0000

    Edits to day_3 solution

--- 2025/day_3/aoc3.js
+++ 2025/day_3/aoc3.js
@@ -5,23 +5,8 @@ import { readFileSync } from 'node:fs';
 var input = readFileSync("./input.txt", "utf8");
 var banks = input.split("\n")
 
-
-
-
-
-
-
-
-
-
-/* First Attempt
-
 let joltageTotal = 0
-
-console.log(banks)
-
 banks.forEach(srch)
-
 console.log(joltageTotal)
 
 function srch(item){
@@ -41,14 +26,12 @@ function srch(item){
     for (let x = 9; x > 0; x--){
         int1 = item.indexOf(x)
         if (int1!=-1 && int1<(item.length - 11)){
-            console.log(int1, item[int1])
             break}
     }
     
     for (let x = 9; x > 0; x--){
         int2 = item.indexOf(x, int1+1)
         if (int2 !=-1 && int2<(item.length - 10)){
-            console.log(int2,item[int2])
             break
         }
     }
@@ -56,7 +39,6 @@ function srch(item){
     for (let x = 9; x > 0; x--){
         int3 = item.indexOf(x, int2+1)
         if (int3 !=-1 && int3<(item.length - 9)){
-            console.log(int3,item[int3])
             break
         }
     }
@@ -64,7 +46,6 @@ function srch(item){
     for (let x = 9; x > 0; x--){
         int4 = item.indexOf(x, int3+1)
         if (int4 !=-1 && int4<(item.length - 8)){
-            console.log(int4,item[int4])
             break
         }
     }
@@ -72,7 +53,6 @@ function srch(item){
     for (let x = 9; x > 0; x--){
         int5 = item.indexOf(x, int4+1)
         if (int5 !=-1 && int5<(item.length - 7)){
-            console.log(int5,item[int5])
             break
         }
     }
@@ -80,7 +60,6 @@ function srch(item){
     for (let x = 9; x > 0; x--){
         int6 = item.indexOf(x, int5+1)
         if (int6 !=-1 && int6<(item.length - 6)){
-            console.log(int6,item[int6])
             break
         }
     }
@@ -88,7 +67,6 @@ function srch(item){
     for (let x = 9; x > 0; x--){
         int7 = item.indexOf(x, int6+1)
         if (int7 !=-1 && int7<(item.length - 5)){
-            console.log(int7,item[int7])
             break
         }
     }
@@ -96,7 +74,6 @@ function srch(item){
     for (let x = 9; x > 0; x--){
         int8 = item.indexOf(x, int7+1)
         if (int8 !=-1 && int8<(item.length - 4)){
-            console.log(int8,item[int8])
             break
         }
     }
@@ -104,7 +81,6 @@ function srch(item){
     for (let x = 9; x > 0; x--){
         int9 = item.indexOf(x, int8+1)
         if (int9 !=-1 && int9<(item.length - 3)){
-            console.log(int9,item[int9])
             break
         }
     }
@@ -112,7 +88,6 @@ function srch(item){
     for (let x = 9; x > 0; x--){
         int10 = item.indexOf(x, int9+1)
         if (int10 !=-1 && int10<(item.length - 2)){
-            console.log(int10,item[int10])
             break
         }
     }
@@ -120,7 +95,6 @@ function srch(item){
     for (let x = 9; x > 0; x--){
         int11 = item.indexOf(x, int10+1)
         if (int11 !=-1 && int11<(item.length-1)){
-            console.log(int11,item[int11])
             break
         }
     }
@@ -128,16 +102,12 @@ function srch(item){
     for (let x = 9; x > 0; x--){
         int12 = item.indexOf(x, int11+1)
         if (int12 !=-1){
-            console.log(int12,item[int12])
             break
         }
     }
     
     let joltage = Number(item[int1].concat("", item[int2], item[int3], item[int4], item[int5], item[int6], item[int7], item[int8], item[int9], item[int10], item[int11], item[int12]))
-    console.log(joltage)
     joltageTotal += joltage
 }
-*/
-
 console.timeEnd('time')
 
