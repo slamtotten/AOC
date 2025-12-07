2b15f4bbfed9e477bc35879c2c4cfe179014badf
slamtotten <slamtotten@gmail.com>
2025-12-07 22:17:54 +0000

    - Initial commit of 2015 Day 5 solution - minor edits to 2025 Day 7 solution

---
+++ 2015/day_5/aoc5.js
@@ -0,0 +1,44 @@
+import { readFileSync } from "fs"
+var input = readFileSync("input.txt","utf8").split("\n")
+
+//Part A
+var ns = 0
+for(var line of input){
+    if(chkVwl(line) == true && chkDbl(line) == true && chkSp(line) == false){ns++}
+    }
+console.log("Nice Strings: ",ns)
+
+function chkVwl(str){
+    var vowels = str.split("").filter(r=>/[aeiou]/g.test(r))
+    if(vowels.length>=3){return true}else{return false}
+}
+function chkDbl(str){
+    if(str.search(/([a-z])\1/) != -1){return true}else{return false}
+}
+function chkSp(str){
+    if(str.search(/(ab|cd|pq|xy)/) != -1){return true}else{return false}
+}
+console.log("Part A: ", ns)
+
+//Part B
+
+var ns = 0
+for(var line of input){
+    if(cnd1(line) == true && cnd2(line) == true){ns++}
+    }
+console.log("Part B: ",ns)
+
+function cnd1(str){
+    if(str.search(/([a-z])([a-z])([a-z]+)?\1\2/) != -1){return true}else{return false}
+}
+function cnd2(str){
+    if(str.search(/([a-z])([a-z])\1/) != -1){return true}else{return false}
+}
+/*regex Reference
+
+vowels = /[aeiou]/
+double letters = /([a-z])\1/
+specific pairs = /(ab|cd|pq|xy)/
+
+
+*/
--- 2025/day_7/aoc7.js
+++ 2025/day_7/aoc7.js
@@ -17,11 +17,11 @@ console.log("Total timelines: ",beam.reduce((a,b) => a+b))
 
 function scnLine(line){
     for(let ind = 0; ind<line.length; ind++){
-            if (line[ind] == "^" && beam[ind]>0){
-                totalSplits++
-                beam[ind-1]+= beam[ind]
-                beam[ind+1]+= beam[ind]
-                beam[ind] = 0
+        if (line[ind] == "^" && beam[ind]>0){
+            totalSplits++
+            beam[ind-1]+= beam[ind]
+            beam[ind+1]+= beam[ind]
+            beam[ind] = 0
             }
         }
     }
