f12cdc433c8ba85605fe6690cb635f286740209c
slamtotten <slamtotten@gmail.com>
2025-12-08 23:47:06 +0000

    Updated 2015 Day 1 solution

--- 2015/day_01/01.js
+++ 2015/day_01/01.js
@@ -13,16 +13,10 @@ console.log(count);
 
 // part 2
 let floor = 0
-for (let ind = 0; ind < input.length; ind++){
-    
-    if (input[ind] == up){
-        floor++
-    } else if (input[ind] == dn){
-        floor--
-        if (floor < 0){
-            console.log(ind+1);
-            break
-        }
+for (let x = 0; x < input.length; x++){
+    if (input[x]==up){floor++}
+    else {floor--}
+    if (floor < 0){console.log(x+1); break}
     }
-}
+
 console.timeEnd()
