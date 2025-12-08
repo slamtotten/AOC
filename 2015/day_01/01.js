cc8373c38e0330874513c9f1007eefe272664e1f
slamtotten <slamtotten@gmail.com>
2025-12-08 23:40:27 +0000

    Updated 2015 Day 1 solution

--- 2015/day_01/01.js
+++ 2015/day_01/01.js
@@ -1,36 +1,24 @@
-console.time()
 import puz from "../../input.js"
 var input = puz
 
-/*  part 1
+// Part A
 var up ="("
 var dn =")"
 
 let count = 0
-let pos = 0
-
-while ((pos = input.indexOf(up, pos)) !== -1){
-    count++
-    pos += up.length;
-}
-
-while ((pos = input.indexOf(dn, pos)) !== -1){
-    count--
-    pos += dn.length;
+for(let x = 0; x<input.length; x++){
+    if (input[x]==up){count++}else {count--}
 }
 console.log(count);
-*/
 
 // part 2
 let floor = 0
 for (let ind = 0; ind < input.length; ind++){
     
-    if (input[ind] == "("){
+    if (input[ind] == up){
         floor++
-        console.log(floor)
-    } else if (input[ind] == ")"){
+    } else if (input[ind] == dn){
         floor--
-        console.log(floor)
         if (floor < 0){
             console.log(ind+1);
             break
