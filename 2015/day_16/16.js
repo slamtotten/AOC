0ce0377cbb9929e8f55c907e081fdaccb7b23273
slamtotten <slamtotten@gmail.com>
2026-01-09 09:02:28 +0000

    Day 16 solution commit

---
+++ 2015/day_16/16.js
@@ -0,0 +1,50 @@
+import {readFileSync} from 'fs'
+const options = readFileSync('input.txt','utf-8').split('\n')
+const sample = readFileSync('test.txt','utf-8').split('\n')
+
+let target = {}
+getProps(target, sample)
+
+let sues = []
+for(let line of options){
+    let sue = {}
+    let num = line.match(/Sue (\d+)/).slice(1).shift()
+    sue.num = num
+    let props = line.split(",")
+    getProps(sue, props)
+    sues.push(sue)
+}
+
+sues.forEach(r =>{
+    if(chkMatchA(r) == true){console.log("Part A:",r.num)}
+    if(chkMatchB(r) == true){console.log("Part B:",r.num)}
+})
+    
+function chkMatchA(sue){
+    for(let key in sue){
+        if(key == "num"){continue}
+        if(sue[key] != target[key]){return false}
+    }
+    return true
+}
+
+function chkMatchB(sue){
+    for(let key in sue){
+        if(key == "num"){continue}
+        if(key == "cats"||key =="trees"){
+            if(+sue[key] <= +target[key]){return false}
+        }
+        else if(key == "goldfish"||key == "pomeranians"){
+            if(+sue[key] >= +target[key]){return false}
+        }
+        else if(sue[key] != target[key]){return false}
+    }
+    return true
+}
+
+function getProps(target, arr){
+    for(let line of arr){
+        let x = line.match(/(?<key>\w+): (?<val>\d+)/).groups
+        target[x.key] = x.val
+    }
+}
