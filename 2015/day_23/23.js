e87fd720b3ccf89338246f0d13e0844e194ac630
slamtotten <slamtotten@gmail.com>
2026-01-14 17:00:53 +0000

    Day 23 solution commit

---
+++ 2015/day_23/23.js
@@ -0,0 +1,28 @@
+import puz from '../../input.js'
+let input = puz.split('\n')
+
+let regs = {a: 0, b: 0}
+run(parseInsts(input))
+
+function parseInsts(arr){
+    let io = {}
+    for (let i = 0; i<arr.length;i++){
+        let x = arr[i].match(/(?<func>\w+)\s(?:(?<reg>\w)(?:,)?)?\s?(?<offset>(?:-|\+)\d+)?/).groups
+        io[i] = {func: x.func, reg: x.reg, offset: +x.offset}
+    }
+    return io
+}
+
+function run(instructions){
+    for (let i = 0; i<input.length;i++){
+        let inst = instructions[i]
+        if (inst.func == "hlf"){regs[inst.reg]/=2}
+        if (inst.func == "tpl"){regs[inst.reg]*=3}
+        if (inst.func == "inc"){regs[inst.reg]++}
+        if (inst.func == "jmp"){i += (inst.offset-1)}
+        if (inst.func == "jie"){if (regs[inst.reg]%2 === 0){i += (inst.offset-1)}}
+        if (inst.func == "jio"){if (regs[inst.reg] === 1){i += (inst.offset-1)}}
+    }
+    console.log(regs["b"])
+}
+
