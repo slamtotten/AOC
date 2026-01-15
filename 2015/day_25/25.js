3d37fcfb35c24d913cf9e9a0769304bacaa7bc00
slamtotten <slamtotten@gmail.com>
2026-01-15 17:16:27 +0000

    variable label change for clarity

--- 2015/day_25/25.js
+++ 2015/day_25/25.js
@@ -1,18 +1,18 @@
 import puz from '../../input.js'
 let codePos = puz.match(/row (?<row>\d+), column (?<col>\d+)/).groups
 
-let init = 20151125
+let code = 20151125
 
 let totRows = +codePos.col + +codePos.row -1
 let totCodes = sigma(totRows)
 let codeNum = totCodes - (totRows - codePos.col)
 
 do{
-    let nc = codeAlgo(init)
-    init = nc
+    let nc = codeAlgo(code)
+    code = nc
     codeNum--
 }while(codeNum>1)
-console.log(init)
+console.log(code)
 
 function codeAlgo(pc){
     return (pc*252533)%33554393
