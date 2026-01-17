9f71b40746a978912d99eded8499771004f75af7
slamtotten <slamtotten@gmail.com>
2026-01-17 22:40:39 +0000

    Day 1 solution commit

---
+++ 2016/day_01/01.js
@@ -0,0 +1,32 @@
+import puz from '../../input.js'
+const seq = puz.split(', ').map(r => {
+    let x = r.match(/(?<turn>\w)(?<dist>\d+)/).groups
+    return {turn: x.turn, dist: +x.dist}
+})
+
+let [x,y,dir] = [0,0,0]
+let [visited, revisit] =[[],[]]
+
+for(let move of seq){
+    dir = turn(dir,move.turn)
+    for(let m = 0;m<move.dist;m++){
+        if(dir == 0){y++}
+        if(dir == 1){x++}
+        if(dir == 2){y--}
+        if(dir == 3){x--}
+        if(!visited.includes([x,y].toString())){visited.push([x,y].toString())}
+        else{revisit.push([x,y])}
+    }
+}
+
+console.log(`Part A: ${Math.abs(x) + Math.abs(y)}`)
+let b = revisit.shift()
+console.log(`Part B: ${b}, ${Math.abs(b[0])+Math.abs(b[1])}`)
+
+function turn(cdir, turn){
+    if(turn == "L"){cdir--}
+    if(turn == "R"){cdir++}
+    if(cdir == -1){cdir = 3}
+    if(cdir == 4){cdir = 0}
+    return cdir
+}
