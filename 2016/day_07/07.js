b309038708776938793ca2c9a852f7fdff46a1fa
slamtotten <slamtotten@gmail.com>
2026-01-18 22:12:31 +0000

    Day 07 minor cleanup

--- 2016/day_07/07.js
+++ 2016/day_07/07.js
@@ -1,8 +1,7 @@
 import puz from '../../input.js'
 let ips = puz.split('\n')
 
-let tls = 0
-let ssl = 0
+let [tls,ssl] = [0,0]
 for(let ip of ips){
     if (ip.match(/(\w)(\w)(?<!\1)\2\1/)!= null && ip.match(/\[(?:\w+)?(\w)(\w)(?<!\1)\2\1(?:\w+)?\]/) == null){tls++}
     if (ip.match(/(\w)(?!\1)(\w)\1\w*(\[|\])(?:\w*(\]|\[)\w*(\[|\]))*\w*\2\1\2/) != null){ssl++}
